4d1f3d5bb3c06bb9c42a614701964aaa58360a40
Firdosh01 <firdosh7392@gmail.com>
2023-04-24 04:01:36 -0500

    hero section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import './Home.css'
+
 
 function Home() {
   return (
-    <div>
-      
-        <span>This is Home page </span>
-      
+    <div className='hero-section'>
+        <div className='hero-container'>
+
+            <div className='hero-left-content'>
+                <h6><em>Are you hungry</em></h6>
+                <h1>Don't wait !</h1>
+                <button>Order Now</button>
+            </div>
+
+            <div className='hero-right-img'>
+
+            <img src="/images/pizza.png" alt="pizza-img" />
+            
+            </div>
+
+
+        </div>
     </div>
   )
 }
